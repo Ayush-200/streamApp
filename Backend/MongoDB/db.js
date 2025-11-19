55bda436e545fab3bc9bd6b804538656837dcb63
Ayush Bhatia <ayushbhatia456@gmail.com>
2025-11-19 12:33:41 +0530

    db.js chanegs

--- Backend/MongoDB/db.js
+++ Backend/MongoDB/db.js
@@ -1,24 +1,29 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  try {
+    // Assign the connection to a variable
+    const conn = await mongoose.connect(
+      "mongodb+srv://ayushbhatia456:Ayushbhatia123@cluster0.denecxq.mongodb.net/yourDBName?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-    try {
-
-        await mongoose.connect("mongodb+srv://ayushbhatia456:Ayushbhatia123@cluster0.denecxq.mongodb.net/?appName=Cluster0", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        // console.log("mongo uri is ", process.env.MONGO_URI);
-        console.log("mongodb connected!", mongoose.connection.name);
-         console.log("Cluster host:", conn.connection.host); // cluster host
-    console.log("Cluster port:", conn.connection.port); // usually 27017 for Atlas
-    console.log("Database name:", conn.connection.name); // database you are connected to
+    console.log("MongoDB connected!");
+    console.log("Cluster host:", conn.connection.host); // cluster host
+    console.log("Cluster port:", conn.connection.port); // usually 27017
+    console.log("Database name:", conn.connection.name); // database name
     console.log("Client info:", conn.connection.client.s.options.srvHost); // Atlas cluster URL
-        const collections = await mongoose.connection.db.listCollections().toArray();
-        console.log("Collections in DB:", collections.map(c => c.name));
-    }
-    catch (error) {
-        console.log("error occured in db.js", error);
-    }
-}
-export default connectDB; 
+
+    // List all collections
+    const collections = await conn.connection.db.listCollections().toArray();
+    console.log("Collections in DB:", collections.map(c => c.name));
+
+  } catch (error) {
+    console.error("Error occurred in db.js", error);
+  }
+};
+
+export default connectDB;
