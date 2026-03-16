f0a45bf2dea12c4c3d35de80300e2a7d48d379e3
Ayush Bhatia <ayushbhatia456@gmail.com>
2026-03-16 10:59:53 +0530

    upload field added to dexie indexing

--- Frontend/my_meeting_app/src/db/db.js
+++ Frontend/my_meeting_app/src/db/db.js
@@ -2,6 +2,6 @@
 import { Dexie } from "dexie"
 
 export const db = new Dexie("myDatabase")
-db.version(1).stores({
-  chunks: "++id, userId, blob, meetingId", // Primary key and indexed props
-})
+db.version(2).stores({
+  chunks: "++id, userId, meetingId, uploaded, chunkIndex" // Primary key and indexed props
+});
